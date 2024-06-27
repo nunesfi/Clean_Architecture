8420102974ccb0e5b7fb75c83f757032f2fbdbf0
nunesfi <so.jc@live.com>
2024-06-26 23:53:20 -0300

    feat: Implementing select and testes in UserRepository

--- src/infra/db/entities/users.py
+++ src/infra/db/entities/users.py
@@ -1,14 +1,15 @@
 from sqlalchemy import Column, String, Integer
 from src.infra.db.settings.base import Base
 
+
 class Users(Base):
-    __tablename__ ="users"
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    first_name = Column(String, nullable= False)
-    last_name = Column(String, nullable= False)
-    age = Column(Integer, nullable= False)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    age = Column(Integer, nullable=False)
 
-    #função para tratamento de resultado
+    # função para tratamento de resultado
     def __repr__(self):
-        return f"Users [id={self.id}, firts_name{self.first_name}]"
+        return f"Users [id={self.id}, firts_name={self.first_name}]"
